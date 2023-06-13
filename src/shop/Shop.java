f27d46cc82024cbc813ee1029230ae7bdf81d7d1
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-13 22:43:50 +0300

    Changes Shop

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -2,8 +2,8 @@ package shop;
 
 import toys.Toys;
 
-public interface Shop extends Iterable<Toys>{
+public interface Shop extends Iterable<Toys> {
     void put(String str);
+
     Toys getqueue(String str);
-    //PriorityQueue<Toys> getShop();
 }
