3a3269759d86e5db974937c31ecf564f9cbf6393
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-13 17:12:38 +0300

    Add Shop

---
+++ src/shop/Shop.java
@@ -0,0 +1,9 @@
+package shop;
+
+import toys.Toys;
+
+public interface Shop extends Iterable<Toys>{
+    void put(String str);
+    Toys getqueue(String str);
+    //PriorityQueue<Toys> getShop();
+}
