c865707ace59e8dfab0d6c5d0de5cf89cc70d1c8
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-13 17:13:50 +0300

    Add Iterator

---
+++ src/shop/ToysIterator.java
@@ -0,0 +1,26 @@
+package shop;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+import toys.Toys;
+
+public class ToysIterator implements Iterator<Toys> {
+    private int index;
+    private PriorityQueue<Toys> shop;
+
+    public ToysIterator(PriorityQueue<Toys> shop2) {
+        this.shop=shop2;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < shop.size();
+    }
+
+    @Override
+    public Toys next() {
+        //index++;
+        return shop.poll();
+    }
+}
