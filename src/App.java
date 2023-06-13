614b348af257417d4b7b49a3fe2a002f5ef9f8fa
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-13 22:46:20 +0300

    Changes App

--- src/App.java
+++ src/App.java
@@ -1,42 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
+import presenter.Presenter;
+import service.Service;
 import service.ShopService;
-import shop.Shop;
-import shop.ShopToys;
-import toys.Toys;
+import ui.ConsoleUi;
+import ui.View;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Shop sh1 = new ShopToys();
-        ShopService qq = new ShopService();
-        String s, g, t;
-        Deque<String> qstr = new ArrayDeque<String>();
-        String str1 = "0 car 2";
-        String str2 = "1 boat 2";
-        String str3 = "2 doll 6";
-        qstr.add(str1);
-        qstr.add(str2);
-        qstr.add(str3);
-        sh1.put(str3);
-        qq.putlot(str3);
-        sh1.put(str1);
-        qq.putlot(str1);
-        sh1.put(str2);
-        qq.putlot(str2);
-        System.out.println(sh1);
-        System.out.println(qq);
-        s = qq.getLott();
-        g = qq.getLott();
-        t = qq.getLott();
-        System.out.println(s);
-        System.out.println(g);
-        System.out.println(t);
-
-        Deque<Toys> qu = new ArrayDeque<Toys>();
-        qu.add(sh1.getqueue(s));
-        qu.add(sh1.getqueue(g));
-        qu.add(sh1.getqueue(t));
-        System.out.println(qu);
+        View view = new ConsoleUi();
+        Service service = new ShopService();
+        Presenter presenter = new Presenter(view, service);
+        view.start();
     }
 }
