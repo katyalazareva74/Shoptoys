a3fdc3e08aadbfeee6cec1b498b00f137daa4321
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-14 21:05:38 +0300

    Add check and save, load

--- src/ui/ConsoleUi.java
+++ src/ui/ConsoleUi.java
@@ -17,13 +17,49 @@ public class ConsoleUi implements View {
 
     @Override
     public void start() throws ClassNotFoundException, IOException {
-        System.out.println("Введите игрушку по формату:"
-                + "\nid игрушки, название игрушки, количество игрушек, процент выпадения игрушки, в одну строку через пробел:");
-        String str = scanner.nextLine();
-        presenter.addtoy(str);
-        presenter.addstr(str);
-        presenter.lottery();
+        int number = 0, get = 1, proc = 0;
+        boolean loop = true;
+        String str = "";
+        System.out.println("Результаты предыдущего розыгрыша");
+        presenter.loadf();
+        while (loop) {
+            System.out.println("Введите игрушку по формату:"
+                    + "\nid игрушки, название игрушки, количество игрушек, процент выпадения игрушки(без знака %),"
+                    + "\nв одну строку через пробел:");
+            str = scanner.nextLine();
+            if (number == 0)
+                number = presenter.tnumber(str);
+            number = presenter.checks(str, number);
+            if (number == 0) {
+                System.out.println("Что-то пошло не так! Попробуйте еще раз!");
+                System.exit(0);
+            }
+            proc = presenter.checksp(str, proc);
+            System.out.println(proc);
+            if (proc > 100) {
+                System.out.println("Что-то пошло не так! Попробуйте еще раз!");
+                System.exit(0);
+            }
+            System.out.println(number);
+            if ((number != 0) && (proc == 0)) {
+                loop = false;
+            }
+            System.out.println(loop);
+            presenter.addtoy(str);
+            presenter.addstr(str);
+        }
+        while (!loop) {
+            System.out.println("Сколько игрушек участвует в лотерее?");
+            get = scanner.nextInt();
+            if (get <= number)
+                loop = true;
+        }
+        while (get != 0) {
+            presenter.lottery();
+            get--;
+        }
         presenter.showshop();
+        presenter.savef();
     }
 
     @Override
