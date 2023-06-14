b3c688da08038738164ab49da44054cbea3e91d4
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-14 21:07:53 +0300

    Add class SaveLoad

---
+++ src/savefile/SaveLoad.java
@@ -0,0 +1,29 @@
+package savefile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SaveLoad implements OutInput {
+
+    @Override
+    public void savefile(Object stoy) throws FileNotFoundException, IOException {
+        try (ObjectOutputStream savetoys = new ObjectOutputStream(new FileOutputStream("data.dat"))) {
+            savetoys.writeObject(stoy);
+            savetoys.close();
+        }
+    }
+
+    @Override
+    public Object loadfile() throws FileNotFoundException, IOException, ClassNotFoundException {
+        ObjectInputStream loadtoys = new ObjectInputStream(new FileInputStream("data.dat"));
+        Object ltoy = new Object();
+        ltoy = loadtoys.readObject();
+        loadtoys.close();
+        return ltoy;
+    }
+    
+}
