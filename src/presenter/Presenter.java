8550eef8d805535043872bb5b10e698cb6e23609
Ekaterina Lazareva <lazarevakatya@mail.ru>
2023-06-14 21:06:48 +0300

    Add check and save, load

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -1,5 +1,8 @@
 package presenter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import service.Service;
 import ui.View;
 
@@ -21,11 +24,31 @@ public class Presenter {
         service.putlot(str);
     }
 
+    public int checks(String str, int n) {
+        return service.check(str, n);
+    }
+
+    public int checksp(String str, int n) {
+        return service.check1(str, n);
+    }
+
+    public int tnumber(String str) {
+        return service.number(str);
+    }
+
     public void lottery() {
         service.getLott();
     }
 
     public void showshop() {
         view.print(service.show());
+    }
+
+    public void savef() throws FileNotFoundException, IOException {
+        service.savequeue();
+    }
+
+    public void loadf() throws FileNotFoundException, IOException, ClassNotFoundException {
+        service.loadqueue();
     }
 }
